3b2a292fe45caa57f0731b994980607bc29a51ad
Manoj Kumar <manoj9788@gmail.com>
2016-04-18 21:27:11 +1000

    gulp tasks to execute tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-var protractor = require("gulp-protractor").protractor;
 
-gulp.src(["./specs/*.spec"])
-    .pipe(protractor({
-        configFile: "protractor-conf.js",
-    }))
-    .on('error', function(e) { throw e })
+var gulp = require('gulp');
+var gp = require('gulp-protractor');
+
+gulp.task('webdriver-update', gp.webdriver_update);
+
+// Setting up the test task
+gulp.task('run-all', function(done) {
+    gulp.src(['specs/*Spec.js']).pipe(gp.protractor({
+        configFile: 'protractor-conf.js'
+    })).on('error', function(e) {
+        console.log(e)
+    }).on('end', done);
+});
